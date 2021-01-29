f17ce47cc9f19dfb029f03524f8b4696002f381a
Aneesah <aneesah@Browning.local>
2021-01-29 12:13:10 +0000

    adding more endpoints

--- src/main/java/com/spring/app/springApp/AppController.java
+++ src/main/java/com/spring/app/springApp/AppController.java
@@ -3,10 +3,9 @@ package com.spring.app.springApp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
@@ -19,5 +18,48 @@ public class AppController {
     public ResponseEntity<Transactions> payments(@RequestBody Transactions savedTransactions) {
         Transactions save = transactionRepository.save(savedTransactions);
         return new ResponseEntity<>(save, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/updatePayment/{id}", consumes = ("application/json"))
+    public ResponseEntity<Transactions> updatePayments(@PathVariable("id") Long id,
+                                                       @RequestBody Transactions transaction) {
+        Optional<Transactions> transactionOptional = transactionRepository.findById(id);
+
+        if (transactionOptional.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        transaction.setId(id);
+
+        Transactions updatedTransaction = transactionRepository.save(transaction);
+        return new ResponseEntity<>(updatedTransaction, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/allPayments", produces = {"application/json"})
+    public ResponseEntity<Iterable<Transactions>> getAllPayments() {
+        Iterable<Transactions> allTransactions = transactionRepository.findAll();
+        return new ResponseEntity<>(allTransactions, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/payment/{id}", produces = {"application/json"})
+    public ResponseEntity<Optional<Transactions>> getPaymentById(@PathVariable("id") Long id) {
+        Optional<Transactions> transaction = transactionRepository.findById(id);
+
+        if(transaction.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/deleteAllPayments")
+    public ResponseEntity<Transactions> deleteAllPayments() {
+        transactionRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/deletePayment/{id}")
+    public ResponseEntity<Transactions> deletePaymentById(@PathVariable("id") Long id) {
+        transactionRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/test/java/com/spring/app/springApp/AppControllerTest.java
+++ src/test/java/com/spring/app/springApp/AppControllerTest.java
@@ -9,12 +9,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AppController.class)
 class AppControllerTest {
@@ -40,7 +44,7 @@ class AppControllerTest {
     }
 
     @Test
-    public void paymentsEndpointReturns201Response2() throws Exception {
+    public void paymentsPostPaymentsEndpointReturns201Response2() throws Exception {
         //given
         Transactions expectedTransactions = Transactions.builder()
                 .accountNumber(12345)
@@ -49,6 +53,7 @@ class AppControllerTest {
 
         when(transactionRepository.save(any(Transactions.class))).thenReturn(expectedTransactions);
 
+        //when
         mockMvc.perform(post("/payments")
                 .contentType("application/json")
                 .content("{\"accountNumber\":12345, \"amount\": 1.23}"))
@@ -56,6 +61,125 @@ class AppControllerTest {
                 .andExpect(jsonPath("$.accountNumber").value(12345))
                 .andExpect(jsonPath("$.amount").value(1.23));
 
+        //then
         verify(transactionRepository, times(1)).save(any(Transactions.class));
+    }
+
+    @Test
+    public void paymentsUpdatePaymentEndpointReturns200Response() throws Exception {
+        //given
+        Transactions expectedTransaction = Transactions.builder()
+                .id(1L)
+                .accountNumber(12345)
+                .amount(new BigDecimal("1.23"))
+                .build();
+
+        when(transactionRepository.findById(1L)).thenReturn(Optional.of(expectedTransaction));
+        when(transactionRepository.save(any(Transactions.class))).thenReturn(expectedTransaction);
+
+        //when
+        mockMvc.perform(put("/payments")
+                .contentType("application/json")
+                .content("{\"id\": 1, \"accountNumber\":12345, \"amount\": 3.23}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.is").value(1))
+                .andExpect(jsonPath("$.accountNumber").value(12345))
+                .andExpect(jsonPath("$.amount").value(3.23));
+
+        //then
+        verify(transactionRepository, times(1)).findById(1L);
+        verify(transactionRepository, times(1)).save(any(Transactions.class));
+    }
+
+    @Test
+    public void paymentsGetPaymentsEndpointReturns200() throws Exception {
+        //given
+        Transactions transaction1 = Transactions.builder()
+                .accountNumber(12345)
+                .amount(new BigDecimal("1.23"))
+                .build();
+
+        Transactions transaction2 = Transactions.builder()
+                .accountNumber(678910)
+                .amount(new BigDecimal("33.40"))
+                .build();
+
+        when(transactionRepository.findAll()).thenReturn(Arrays.asList(transaction1, transaction2));
+
+        //when
+        mockMvc.perform(get("/allPayments"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].accountNumber").value(12345))
+                .andExpect(jsonPath("$[0].amount").value(1.23))
+                .andExpect(jsonPath("$[1].accountNumber").value(678910))
+                .andExpect(jsonPath("$[1].amount").value(33.40));
+
+        //then
+        verify(transactionRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void paymentsGetPaymentByIdEndpointReturns200() throws Exception {
+        //given
+        Transactions transaction1 = Transactions.builder()
+                .id(1L)
+                .accountNumber(12345)
+                .amount(new BigDecimal("1.23"))
+                .build();
+
+        when(transactionRepository.findById(1L)).thenReturn(Optional.of(transaction1));
+
+        //when
+        mockMvc.perform(get("/payment/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value(12345))
+                .andExpect(jsonPath("$.amount").value(1.23));
+
+        //then
+        verify(transactionRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    public void paymentsDeleteAllPaymentsEndpointReturns200() throws Exception {
+        //given
+        Transactions transaction1 = Transactions.builder()
+                .id(1L)
+                .accountNumber(12345)
+                .amount(new BigDecimal("1.23"))
+                .build();
+
+        Transactions transaction2 = Transactions.builder()
+                .accountNumber(678910)
+                .amount(new BigDecimal("33.40"))
+                .build();
+
+        transactionRepository.deleteAll(Arrays.asList(transaction1, transaction2));
+
+        //when
+        mockMvc.perform(delete("/deleteAllPayments"))
+                .andExpect(status().isOk());
+
+        //then
+        verify(transactionRepository, times(1)).deleteAll(Arrays.asList(transaction1, transaction2));
+    }
+
+    @Test
+    public void paymentsDeleteByIdEndpointReturns200() throws Exception {
+        //given
+        Transactions transaction1 = Transactions.builder()
+                .id(1L)
+                .accountNumber(12345)
+                .amount(new BigDecimal("1.23"))
+                .build();
+
+        transactionRepository.delete(transaction1);
+
+        //when
+        mockMvc.perform(delete("/deletePayment/{id}", 1))
+                .andExpect(status().isOk());
+
+        //then
+        verify(transactionRepository, times(1)).deleteById(1L);
     }
 }
